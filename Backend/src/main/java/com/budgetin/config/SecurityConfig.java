26d074bffcdf89ee23becd1319940a656bc0dc9d
dindazeinitta <dinda.z.q.p@gmail.com>
2025-07-06 16:20:37 +0700

    update ui

--- Backend/src/main/java/com/budgetin/config/SecurityConfig.java
+++ Backend/src/main/java/com/budgetin/config/SecurityConfig.java
@@ -82,6 +82,8 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5501", "http://localhost:5501", "http://127.0.0.1:5500", "http://localhost:5500", 
         "http://127.0.0.1:3000", 
     "http://localhost:3000",
+    "http://127.0.0.1:3002", 
+    "http://localhost:3002",
     "http://127.0.0.1:3004", 
     "http://localhost:3004"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
